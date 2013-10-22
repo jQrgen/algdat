81e504f6f7cebb3d9d2d4c636fdcced3a79a9dac
jQrgen <jorgen.notland@gmail.com>
2013-10-22 13:30:53 +0200

    hei

--- mumien.py
+++ mumien.py
@@ -1,7 +1,31 @@
 from sys import stdin, stderr
+from sets import Set
 
-#def beste_sti(nm, sans):
-    # SKRIV DIN KODE HER
+def beste_sti(nm, sans):
+    tattlol = [0]
+    tempsans = 0
+    while True:
+        newnode = 0
+        for i in range(0, nm.__len__()):
+            if i in tattlol:
+                for x in range(0, len(nm[i])):
+                    if nm[i][x] != 0 and sans[x] >= tempsans and x not in tattlol:
+                        tempsans = sans[x]
+                        newnode = x
+        tattlol.append(newnode)
+        tempsans = 0
+
+        tilgjenglige_noder = Set()
+        for tatt in tattlol:
+            i = 0
+            for nabo in nm[tatt]:
+                if nabo == 1:
+                    tilgjenglige_noder.add(i)
+                i += 1
+        if(tilgjenglige_noder.__len__() >= nm.__len__()):
+            return tattlol
+            break
+        # SKRIV DIN KODE HER
 
 n = int(stdin.readline())
 sansynligheter = [float(x) for x in stdin.readline().split()]
@@ -12,5 +36,5 @@ for linje in stdin:
     for nabo in naboer:
         naborad[nabo] = 1
     nabomatrise.append(naborad)
-print nabomatrise
-#print beste_sti(nabomatrise, sansynligheter)
+print beste_sti(nabomatrise, sansynligheter)
+# TODO: SPORRE STUDASS HVORFOR DET IKKE ER RIKTIG
